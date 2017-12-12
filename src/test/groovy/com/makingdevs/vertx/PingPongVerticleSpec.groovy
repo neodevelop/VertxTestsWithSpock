1bcb92701694aa303922e62af3c15ec6669315a6
neodevelop <juan@makingdevs.com>
2017-12-11 18:02:21 -0600

    Adding comments for docs

--- src/test/groovy/com/makingdevs/vertx/PingPongVerticleSpec.groovy
+++ src/test/groovy/com/makingdevs/vertx/PingPongVerticleSpec.groovy
@@ -7,12 +7,23 @@ import spock.util.concurrent.PollingConditions
 @Stepwise
 class PingPongVerticleSpec extends Specification {
 
+  // We use the vertx instance provided
   @Shared Vertx vertx
+  // Where are the verticle files?
   @Shared String baseDir = "com/makingdevs/vertx"
+  // The verticle filename
   @Shared String verticleName = "ReceiverVerticle.groovy"
+  // We use this id for deploy and undeploy
   @Shared String verticleId
+  // Look ma! The async magic...!
   PollingConditions conditions = new PollingConditions()
 
+  /*
+  * For all the specs inside this file (executing once):
+  * - Deploying the verticle with the vertx instance
+  * - `assert` the deployment identifier
+  * - Waiting for deployment
+  */
   def setupSpec() {
     vertx = Vertx.vertx()
     vertx.deployVerticle("${baseDir}/${verticleName}") { deployResponse ->
@@ -22,6 +33,12 @@ class PingPongVerticleSpec extends Specification {
     Thread.sleep 1000
   }
 
+  /*
+  * At the end of all the specs:
+  * - Waiting for attend all the messages
+  * - Undeploying the verticle
+  * - Closing vertx resource
+  */
   def cleanupSpec() {
     Thread.sleep 2000
     vertx.undeploy(verticleId) { response ->
@@ -31,19 +48,19 @@ class PingPongVerticleSpec extends Specification {
   }
 
   def "Making Ping to a Verticle"(){
-    given:
+    given: "A message for send, and an empty response"
       def message = "Ping!"
       def response = ""
-    and:
+    and: "A consumer in vertx for waiting the response"
       vertx.eventBus().consumer("com.makingdevs.pong") { msg ->
         response = msg.body()
       }
-    when:
+    when: "We send a message, and wait..."
       vertx.eventBus().send("com.makingdevs.ping", message)
-      conditions.eventually {
+      conditions.eventually { // This should happen...
         assert response == "Pong!"
       }
-    then:
+    then: "We shouldn't thrown exception because conditions was satisfied..."
       noExceptionThrown()
   }
 
